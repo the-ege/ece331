71c4c10bda0089360007e99a47e9a585308915a9
Seth <seth.raymond@maine.edu>
2016-04-17 19:01:10 -0400

    Printing all months. Had to add in a 6th row.

--- hw9/index.php
+++ hw9/index.php
@@ -11,22 +11,22 @@ if (array_key_exists("year",$_POST)) {
 }
 
 function mkcal($year) {
-	$d=new DateTime('now');
-	$ny=$d->format('Y');
+	$today=new DateTime('now');
+	$ny=$today->format('Y');
 	
 	#Get start of year
 	$k=new DateTime('first day of January' . date('Y'));
 	$sdom=$k->format('j'); #starting dom
-	$smonth=$d->format('F'); #text of starting month
+	$smonth=$today->format('F'); #text of starting month
 	
 	if ($ny>$year) {
 		$x=$ny-$year;
-		$d->sub(new DateInterval('P' . $x . 'Y'));
+		$today->sub(new DateInterval('P' . $x . 'Y'));
 	} else {
 		$x=$year-$ny;
-		$d->add(new DateInterval('P' . $x . 'Y'));
+		$today->add(new DateInterval('P' . $x . 'Y'));
 	}
-	for ($i=0;$i<12;$i++) {
+	for ($i=1;$i<13;$i++) {
 		$dom=$k->format('j'); #dom w/o leading zeros
 		$month=$k->format('F'); #text of month
 		$year=$k->format('Y'); #4 digit year
@@ -36,12 +36,12 @@ function mkcal($year) {
 		print "<TABLE Border=1>";
 		print "<TR><TH COLSPAN=7>$month $year</TR>";
 		print "<TR><TH>SUN<TH>MON<TH>TUE<TH>WED<TH>THU<TH>FRI<TH>SAT</TR>";
-		for ($j=0;$j<7*5;$j++) {
+		for ($j=0;$j<7*6;$j++) {
 			if ($j%7==0) {
 				print "<TR>";
 			}
 			$cdom=$k->format('j');
-			$checkdom=$d->format('j');
+			$checkdom=$today->format('j');
 			if ($dom==$checkdom) {
 				print "<TD ALIGN='CENTER' BGCOLOR='LightGreen'>$cdom";
 			} else if ($j<7 && $cdom>7) { #make bg for previous month days, no bg if month starts on Sun
@@ -56,9 +56,9 @@ function mkcal($year) {
 			}
 			$k->add(new DateInterval('P1D'));
 		}
-		if ($cdom>1) {
-			$k->modify('first day of next month');
-		}
+		
+		$k->setDate($year,$i+1,1);
+			
 		print "</TABLE>";
 	}
 }
